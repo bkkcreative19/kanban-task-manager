5cc3fe61725f7cdc9adc90e0d0461275f12cf489
bkkcreative19 <bkkcreative16@gmail.com>
2022-07-26 15:26:45 -0500

    contiuned drag and drop

--- client/src/Home/BoardLists/BoardList/index.js
+++ client/src/Home/BoardLists/BoardList/index.js
@@ -5,22 +5,6 @@ import { DragDropContext, Droppable } from "react-beautiful-dnd";
 import { useState } from "react";
 
 const BoardList = ({ column, index }) => {
-  const [test, setTest] = useState(column);
-  console.log(test);
-
-  const onDragEnd = (result) => {
-    if (!result.destination) return;
-    const { source, destination } = result;
-
-    const copiedItems = [...test.tasks];
-    const [removed] = copiedItems.splice(source.index, 1);
-    copiedItems.splice(destination.index, 0, removed);
-    setTest({
-      ...test,
-      tasks: copiedItems,
-    });
-  };
-
   return (
     <List>
       <ListHead>
@@ -29,20 +13,19 @@ const BoardList = ({ column, index }) => {
           {column.name} ({column.tasks.length})
         </ListHeadText>
       </ListHead>
-      <DragDropContext onDragEnd={(result) => onDragEnd(result)}>
-        <Droppable droppableId={String(index)} key={index}>
-          {(provided, snapshot) => {
-            return (
-              <div {...provided.droppableProps} ref={provided.innerRef}>
-                {test.tasks.map((task, idx) => {
-                  return <BoardTask key={idx} task={task} id={idx} />;
-                })}
-                {provided.placeholder}
-              </div>
-            );
-          }}
-        </Droppable>
-      </DragDropContext>
+
+      <Droppable droppableId={String(index)} key={index}>
+        {(provided, snapshot) => {
+          return (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              {column.tasks.map((task, idx) => {
+                return <BoardTask key={idx} task={task} id={idx} />;
+              })}
+              {provided.placeholder}
+            </div>
+          );
+        }}
+      </Droppable>
     </List>
   );
 };
--- client/src/Home/BoardLists/index.js
+++ client/src/Home/BoardLists/index.js
@@ -5,35 +5,58 @@ import BoardList from "./BoardList";
 import { Lists } from "./Styles";
 import { getBoardWithColumns } from "../../shared/api/boardsApi";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
 
 const BoardLists = ({ active }) => {
   const queryClient = useQueryClient();
-  // const [board, setBoard] = useState(null);
+
   const {
     isLoading,
     isError,
     data: board,
   } = useQuery(["board", active], () => getBoardWithColumns(active));
 
-  // useEffect(() => {
-  //   const test = async () => {
-  //     const { data } = await axios.get(
-  //       `http://localhost:5000/boards/${actives[0]}`
-  //     );
+  const [test, setTest] = useState();
 
-  //     setBoard(data[0]);
-  //   };
+  const onDragEnd = (result) => {
+    if (!result.destination) return;
+    const { source, destination } = result;
 
-  //   test();
-  // }, [actives]);
+    if (source.droppableId !== destination.droppableId) {
+      const sourceColumn = board.columnTypes[source.droppableId];
+      const destColumn = board.columnTypes[destination.droppableId];
+
+      const sourceItems = sourceColumn.tasks;
+      const destItems = destColumn.tasks;
+
+      const [removed] = sourceItems.splice(source.index, 1);
+      destItems.splice(destination.index, 0, removed);
+    } else {
+      const column = board.columnTypes[source.droppableId];
+      const copiedItems = column.tasks;
+      const [removed] = copiedItems.splice(source.index, 1);
+      copiedItems.splice(destination.index, 0, removed);
+    }
+
+    // console.log(result);
+    // const copiedItems = [...test.tasks];
+    // const [removed] = copiedItems.splice(source.index, 1);
+    // copiedItems.splice(destination.index, 0, removed);
+    // setTest({
+    //   ...test,
+    //   tasks: copiedItems,
+    // });
+  };
 
   return (
     <>
       <Lists>
-        {board &&
-          board.columnTypes.map((column, idx) => {
-            return <BoardList key={idx} column={column} index={idx} />;
-          })}
+        <DragDropContext onDragEnd={(result) => onDragEnd(result)}>
+          {board &&
+            board.columnTypes.map((column, idx) => {
+              return <BoardList key={idx} column={column} index={idx} />;
+            })}
+        </DragDropContext>
       </Lists>
     </>
   );
