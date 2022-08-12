3e6f882da5b82b6674fd3479039946f4792168c5
bkkcreative19 <bkkcreative16@gmail.com>
2022-08-12 18:09:42 -0500

    h

--- api/build/database/connection.js
+++ api/build/database/connection.js
@@ -28,11 +28,7 @@ const typeorm_1 = require("typeorm");
 const entities = __importStar(require("../entities"));
 const AppDataSource = new typeorm_1.DataSource({
     type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "basketball10",
-    database: process.env.DB_DATABASE,
+    url: "postgres://hvmceoqdpdhzvu:5cda65b896ad178bd2d727d78edf4d24b7b5ebc7595cf5f786853accb141aae2@ec2-34-233-115-14.compute-1.amazonaws.com:5432/d5fm1ajpotufnt",
     entities: Object.values(entities),
     synchronize: true,
 });
