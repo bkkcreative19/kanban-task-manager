bede568843a2c0d5ca88739d8eb14e2c06f3fb2c
bkkcreative19 <bkkcreative16@gmail.com>
2022-08-13 17:12:57 -0500

    rgae

--- api/build/index.js
+++ api/build/index.js
@@ -12,8 +12,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv/config");
-require("reflect-metadata");
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const morgan_1 = __importDefault(require("morgan"));
