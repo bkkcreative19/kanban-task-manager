63c30e9b913a5137acb2b991f8a53888b30ffe45
bkkcreative19 <bkkcreative16@gmail.com>
2022-08-15 15:54:56 -0500

    api url

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -4,24 +4,24 @@ import * as columns from "./controller/columns";
 import * as subtasks from "./controller/subtasks";
 
 export const routes = (app: any): void => {
-  app.get("/boards", boards.getBoards);
-  app.get("/boards/:boardId", boards.getBoardWithColumns);
-  app.delete("/boards/:boardId", boards.deleteBoard);
+  app.get("/api/boards", boards.getBoards);
+  app.get("/api/boards/:boardId", boards.getBoardWithColumns);
+  app.delete("/api/boards/:boardId", boards.deleteBoard);
 
-  app.post("/boards", boards.createBoardWithColumns);
-  app.put("/boards/:boardId", boards.editBoardWithColumns);
+  app.post("/api/boards", boards.createBoardWithColumns);
+  app.put("/api/boards/:boardId", boards.editBoardWithColumns);
 
   // task routes
-  app.get(`/tasks/:title`, tasks.getTaskAndSubtasks);
-  app.post(`/tasks`, tasks.createTaskWithSubtasks);
-  app.put(`/tasks/:taskId`, tasks.editTaskWithSubtasks);
-  app.delete(`/tasks/:taskTitle`, tasks.deleteTask);
+  app.get(`/api/tasks/:title`, tasks.getTaskAndSubtasks);
+  app.post(`/api/tasks`, tasks.createTaskWithSubtasks);
+  app.put(`/api/tasks/:taskId`, tasks.editTaskWithSubtasks);
+  app.delete(`/api/tasks/:taskTitle`, tasks.deleteTask);
 
   // subtask routes
-  app.delete("/subtasks/:subtaskId", subtasks.deleteSubtask);
+  app.delete("/api/subtasks/:subtaskId", subtasks.deleteSubtask);
   // column routes
-  app.get("/columns/:boardId", columns.getColumns);
-  app.post("/columns/:boardId", columns.addColumn);
-  app.put("/columns/:boardId", columns.updateColumns);
-  app.delete("/columns/:columnId", columns.deleteColumn);
+  app.get("/api/columns/:boardId", columns.getColumns);
+  app.post("/api/columns/:boardId", columns.addColumn);
+  app.put("/api/columns/:boardId", columns.updateColumns);
+  app.delete("/api/columns/:columnId", columns.deleteColumn);
 };
