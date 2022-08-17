4f3d3be68a191237f3076c2353d94fa98a489233
bkkcreative19 <bkkcreative16@gmail.com>
2022-08-16 20:38:19 -0500

    changed caddyfile

--- client/src/shared/features/api/apiSlice.js
+++ client/src/shared/features/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: "https://kris-taskmanager" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://kris-taskmanager/api" }),
   tagTypes: ["Board", "Column", "Task"],
   endpoints: (builder) => ({}),
 });
