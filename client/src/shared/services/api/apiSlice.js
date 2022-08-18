993c2c34c3c1caae86dca60f10c1e75356b36abd
bkkcreative19 <bkkcreative16@gmail.com>
2022-08-18 18:52:13 -0500

    build

--- client/src/shared/services/api/apiSlice.js
+++ client/src/shared/services/api/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const baseUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:5001/api"
-    : "https://kris-taskmanager.com/api";
+    : "https://api-kris-taskmanager.onrender.com/api";
 
 export const apiSlice = createApi({
   reducerPath: "api",
