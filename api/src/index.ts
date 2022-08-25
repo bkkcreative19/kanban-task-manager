1af55fb99f9dfae00cedddebd14c0f43d10cd76d
bkkcreative19 <bkkcreative16@gmail.com>
2022-08-25 17:44:01 -0500

    sf

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import morgan from "morgan";
+// import morgan from "morgan";
 
 import { AppDataSource } from "./database/connection";
 
@@ -20,16 +20,16 @@ const initializeExpress = (): void => {
   const app = express();
   app.use(cors());
   app.use(express.json());
-  app.use(morgan("tiny"));
+  // app.use(morgan("tiny"));
 
   app.get("/", async (_req, res) => {
-    res.send("his");
+    res.send("hiss");
   });
   app.use("/api", router);
 
   const PORT = process.env.PORT || 5000;
 
-  app.listen(5001);
+  app.listen(PORT);
 };
 
 const initializeApp = async (): Promise<void> => {
